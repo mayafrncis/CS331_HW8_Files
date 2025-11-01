1e9204ffd8c4e1b9694bbeb4f353e046361ec136
mayafrncis <maya.ara.francis@gmail.com>
2025-11-01 19:52:57 +0000

    completed

--- q1.c
+++ q1.c
@@ -12,64 +12,43 @@ int main() {
 	char dest[256];
 	printf("Enter source file path: ");
 	scanf("%256s", source);
-	printf("\nEnter destination file path: ");
+	printf("Enter destination file path: ");
 	scanf("%256s", dest);
 
 	int fds, fdd;
 	errno = 0;
-	char c[10]; // where the characters will be stored
-	errno = 0;
+	char c[10];
 	fds = open(source, O_RDONLY);
 	fdd = open(dest, O_WRONLY|O_CREAT|O_TRUNC);
 	if (fds == -1 || fdd == -1) {
 		perror("opening");
 	}
-	printf("\nopened");
-	ssize_t ret;
+
+	ssize_t retr, retw;
 	char * buf;
 	int len;
-	while (1) {
-		buf = c;
-		len = 8;
-		while((ret = read(fds, buf, len)) != 0) {
-			if (ret == -1) {
-				if (errno == EINTR)
-					continue;
-				perror("read");
-				break;
+	buf = c;
+	while ((retr = read(fds, buf, 8)) > 0) { // keeps going until EOF or reading fails
+ 		int written = 0; // how many we've written
+		while (retr > written) { // checks if we have bytes that were read to still write then starts writing from where we last left off in the buffer and writes however many bytes were read
+			retw = write(fdd, buf + written, retr - written);
+			if (retw == -1) {
+				perror("writing");
 			}
-			if (ret == 0) break;
-			printf("\nread 8");
-			len -= ret;
-			buf += ret;
-		}
-		// buf ends up pointing to the next empty spot
-		// len makes sure we keep going until we read 8 characters for sure since the process of reading can be interrupted
-
-		if (ret == 0)
-			break; // reached EOF and read nothing, so we have nothing to write
-		int read = buf - c; // subtracting the addresses from each other gives us how many characters we read and so how many we need to write
-		buf = c; // resetting to start writing from the start
-		printf("\nread: %d", read);
-		while(read > 0) {
-			ret = write(fdd, buf, read);
-			if (ret == -1) {
-				if (errno == EINTR)
-					continue;
-				perror("write");
-				break;
-			}
-			printf("\nwrote 8");
-			read -= ret; // in case we got interrupted and didn't manage to write all, to keep track of how many we have left to write
-			buf += ret;
+			written += retw; // in case it doesn't manage to write all 8 bytes before being interrupted, we would need to keep track of what we still have left to write
 		}
 	}
+
+	if (retr == -1)	{
+		perror("read");
+	} else if (retr == 0) {
+		printf("File contents copied.\n");
+	}
+
 	int cd = close(fdd);
 	int cs = close(fds);
 	if (cd == -1 || cs == -1) {
 		perror("close");
 	}
-	printf("\nclosed\n");
-	printf("%d", errno);
 	return 0;
 }
