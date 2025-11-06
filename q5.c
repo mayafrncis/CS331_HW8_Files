7e3399f51178139490e8b2bc89ab3a85d872272a
mayafrncis <maya.ara.francis@gmail.com>
2025-11-06 19:08:59 +0000

    q5

---
+++ q5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() {
+	int fd = open ("sparse.bin", O_WRONLY|O_CREAT|O_TRUNC, 0644);
+	if (fd == -1) perror("open");
+
+	char s[] = "START";
+	ssize_t ret = write(fd, s, strlen(s));
+	off_t p = lseek(fd, 1024 * 1024, SEEK_CUR);
+	if (p == -1) perror("lseek");
+
+	char e[] = "END";
+	ret = write(fd,e, strlen(e));
+	if (ret == -1) perror("write");
+
+	if (close(fd) == -1) perror("close");
+
+	fd = open ("sparse.bin", O_RDONLY);
+	p = lseek(fd, 0, SEEK_END);
+	if (p == -1) perror("lseek");
+
+	printf("Number of bytes in file: %ld\n", p);
+
+	return 0;
+}
